fbf4221928d5ba6ec8ec6988dd946a7eb67b6f4d
경길 임 <lkko3579@naver.com>
2022-05-31 17:42:04 +0900

    Day2 커밋

---
+++ day01/WpfBikeShop/Car.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Media;
+
+namespace WpfBikeShop
+{
+    public class Car
+    {
+        public double Speed { get; set; }
+        
+        public Color Color { get; set; }
+
+        public Car()
+        {
+
+        }
+    }
+}
