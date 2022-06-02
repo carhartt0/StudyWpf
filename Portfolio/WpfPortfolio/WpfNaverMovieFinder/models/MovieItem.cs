00b1ac0d89548bcf7a19b7db8c846d48f196299a
경길 임 <lkko3579@naver.com>
2022-06-02 17:32:46 +0900

    폴더명 변경

---
+++ Portfolio/WpfPortfolio/WpfNaverMovieFinder/models/MovieItem.cs
@@ -0,0 +1,27 @@
+﻿namespace WpfNaverMovieFinder.models
+{
+    internal class MovieItem
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Image { get; set; }
+        public string SubTitle { get; set; }
+        public string Pubdate { get; set; }
+        public string Director { get; set; }
+        public string Actor { get; set; }
+        public string UserRating { get; set; }
+
+        public MovieItem(string title, string link, string image, string subTitle,
+                         string pubdate, string director, string actor, string userRating)
+        {
+            Title = title;
+            Link = link;
+            Image = image;
+            SubTitle = subTitle;
+            Pubdate = pubdate;
+            Director = director;
+            Actor = actor;
+            UserRating = userRating;
+        }
+    }
+}
