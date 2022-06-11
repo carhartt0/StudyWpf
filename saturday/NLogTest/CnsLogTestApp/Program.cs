9cd0e9732af7fe02e9f9e1b1a69c8fa47d165a7b
경길 임 <lkko3579@naver.com>
2022-06-11 15:47:23 +0900

    20220611 토요일 NLog 학습

---
+++ saturday/NLogTest/CnsLogTestApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CnsLogTestApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 0;
+            while (true)
+            {
+                i++;
+                Console.WriteLine($"Hello world! : {i}");
+                Debug.WriteLine($"Hello world! : {i}");
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
